5d1c57bdac7424bde9fd8951c0d89d6f0f83b446
rbagrin <bagrin.radu@gmail.com>
2021-03-18 00:10:34 +0000

    ADSAD

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
+app.get('/.well-known/acme-challenge/6D94L5iYDuGgNmb0PEPODYBP6U2bLDUhLJu-EWozTY8', (req,res) => {
+  res.send("6D94L5iYDuGgNmb0PEPODYBP6U2bLDUhLJu-EWozTY8.wmdxtLmh9aIt-WrmbqHbZ0g0XeN6pESAsjYtfRo6OXk");
+});
+
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
